e13fe51f7456dd2f5f1ef8a407c1f172fba813df
Chang Xiong <cnxiong03@gmail.com>
2020-02-11 20:43:22 -0500

    error handling

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,26 @@ const router = express.Router();
 const Driver = require('../models/drivers');
 
 // get a list of drivers form the db
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
     res.send({ type: 'GET'});
 });
 
 // add a new driver
-router.post('/drivers', function(req, res) {
+router.post('/drivers', function(req, res, next) {
     // will create and save new driver to db then wait send back new driver to client
     Driver.create(req.body).then(function(driver){
         res.send(driver);
-    });
+    })
+    .catch(next);
 });
 
 // update a driver in the db
-router.put('/drivers/:id', function(req, res) {
+router.put('/drivers/:id', function(req, res, next) {
     res.send({ type: 'PUT' });
 });
 
 // delete a driver from the db
-router.delete('/drivers/:id', function(req, res) {
+router.delete('/drivers/:id', function(req, res, next) {
     res.send({ type: 'DELETE' });
 });
 
